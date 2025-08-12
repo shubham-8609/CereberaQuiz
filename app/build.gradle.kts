75dab3f1e627f26b6252cd44ee3ec66ecc2f7f31
codeleg <shubhamgupta8609@gmail.com>
2025-08-12 17:10:17 +0530

    Created the start page and created cartView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     implementation("com.airbnb.android:lottie:6.6.6")
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("com.intuit.ssp:ssp-android:1.1.1")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
